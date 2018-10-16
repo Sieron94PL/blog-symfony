d0495ce8d1c97f772ff510409b256264d6c300f0
Damian Sieroń <damiansieron94@gmail.com>
2018-10-16 14:16:49 +0200

    [add] Add new, index methods.

--- src/Controller/PostController.php
+++ src/Controller/PostController.php
@@ -5,6 +5,9 @@ namespace App\Controller;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\Routing\Annotation\Route;
 use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
 
 use App\Entity\Post;
 
@@ -15,19 +18,42 @@ class PostController extends AbstractController
      */
     public function index()
     {
+       $posts = $this->getDoctrine()->getRepository(Post::class)->findAll();
 
-       $entityManager = $this->getDoctrine()->getManager();
-
-       $post = new Post();
-       $post->setContent('This is content.');
-
-       $entityManager->persist($post);
-       $entityManager->flush();
-
-        return $this->render('post/index.html.twig', [
-            'controller_name' => 'PostController',
-        ]);
+        return $this->render('post/index.html.twig', array(
+          'posts' => $posts,
+        ));
     }
+
+    /**
+    * @Route("post/new", name="post_new")
+    */
+    public function new(Request $request)
+    {
+      $post = new Post();
+      $post->setContent('Write a post content');
+
+      $form = $this->createFormBuilder($post)
+        ->add('content', TextType::class)
+        ->add('save', SubmitType::class, array('label' => 'Create Post'))
+        ->getForm();
+
+      $form->handleRequest($request);
+
+      if($form->isSubmitted() && $form->isValid()) {
+        $post = $form->getData();
+
+        $entityManager = $this->getDoctrine()->getManager();
+        $entityManager->persist($post);
+        $entityManager->flush();
+      }
+
+        return $this->render('post/new.html.twig', array(
+          'form' => $form->createView(),
+        ));
+    }
+
+
 
     /**
     * @Route("post/{id}", name="post_show")
