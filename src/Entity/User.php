dcbbaa0848191e774fbaa9d52af6a23fd88887fa
Damian Sieroń <damiansieron94@gmail.com>
2018-10-17 17:40:06 +0200

    [add] Add Registration view and change password length in User entity to 64 characters.

--- src/Entity/User.php
+++ src/Entity/User.php
@@ -24,7 +24,7 @@ class User implements UserInterface, \Serializable
     private $username;
 
     /**
-     * @ORM\Column(type="string", length=25)
+     * @ORM\Column(type="string", length=64)
      */
     private $password;
 
